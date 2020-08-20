4670aafb5942fcfcfebc7d3e5ffc732eeb326502
Johan Reitan <johan.reitan@gmail.com>
2020-08-20 21:14:55 +0200

    add inset in menu

--- lib/menu.dart
+++ lib/menu.dart
@@ -31,60 +31,65 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'KARTTYPE',
-            style: Theme.of(context)
-                .textTheme
-                .button
-                .copyWith(fontSize: 12, letterSpacing: 1.3),
-          ),
-          SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: _mapTypes.map((mapType) {
-              final active = mapType['mapType'] == activeMapType;
-              return InkResponse(
-                radius: 70,
-                onTap: () => onChangeMapType(mapType['mapType']),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 80,
-                      height: 80,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        border: active
-                            ? Border.all(
-                                color: Theme.of(context).primaryColor,
-                                width: 2,
-                              )
-                            : null,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(mapType['image']),
+    return SafeArea(
+      minimum:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+        height: 200,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'KARTTYPE',
+              style: Theme.of(context)
+                  .textTheme
+                  .button
+                  .copyWith(fontSize: 12, letterSpacing: 1.3),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: _mapTypes.map((mapType) {
+                final active = mapType['mapType'] == activeMapType;
+                return InkResponse(
+                  radius: 70,
+                  onTap: () => onChangeMapType(mapType['mapType']),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(
+                            color: active
+                                ? Theme.of(context).primaryColor
+                                : Theme.of(context).dividerColor,
+                            width: active ? 2 : 1,
+                          ),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(mapType['image']),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      mapType['text'],
-                      style: Theme.of(context).textTheme.caption.copyWith(
-                            color:
-                                active ? Theme.of(context).primaryColor : null,
-                          ),
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-        ],
+                      SizedBox(height: 8),
+                      Text(
+                        mapType['text'],
+                        style: Theme.of(context).textTheme.caption.copyWith(
+                              color: active
+                                  ? Theme.of(context).primaryColor
+                                  : null,
+                            ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }

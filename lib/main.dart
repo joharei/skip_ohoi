439607f0f2aa64e75d1da3149e7f0ee95699480e
Johan Reitan <johan.reitan@gmail.com>
2020-08-20 21:12:35 +0200

    make system navigation bar transparent

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(children: [
         Map(mapType: mapType),
         Positioned(
